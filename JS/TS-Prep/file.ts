66d1a4d937dd3e2f804721e362ff1158b74bcc2b
U-Dev <udaythakur.shely@gmail.com>
2026-08-06 00:00:03 -0700

    chore: daily backup 2026-08-06

--- JS/TS-Prep/file.ts
+++ JS/TS-Prep/file.ts
@@ -1,3 +1,34 @@
 /** @format */
 
-console.log("Hello world");
+// /** @format */
+
+// if(){
+//   else{
+
+//   }
+// }
+
+// while(){
+
+// }
+
+// do{
+
+// }while();
+
+// for(){
+
+// }
+
+// arrays: length, slice, in***, push, pop, reduce, map, filter, sort
+// strings: length, slice, in***, split, trim, parseInt, toString, push, pop, reduce, map, filter, sort, indexOf
+
+const addA = (a, b) => a + b;
+
+function addB(a, b) {
+  a + b;
+}
+
+// Object
+
+// Classes
