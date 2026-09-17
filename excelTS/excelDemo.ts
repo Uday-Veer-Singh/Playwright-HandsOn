4710ae134cc00c00d7762d828fd0d6a2a6f41d3a
U-Dev <udaythakur.shely@gmail.com>
2026-09-17 12:56:26 -0700

    chore: daily backup 2026-09-17

--- excelTS/excelDemo.ts
+++ excelTS/excelDemo.ts
@@ -2,26 +2,48 @@
 
 import { Workbook } from "exceljs";
 
-async function readExcelFile() {
+async function writeExcelFile(
+  searchText: string,
+  replaceText: string,
+  filePath: string
+) {
   const workbook = new Workbook();
-  await workbook.xlsx.readFile(
-    "C:/Users/Acer/OneDrive/Desktop/excelDownload.xlsx"
-  );
+  await workbook.xlsx.readFile(filePath);
 
   const sheet = workbook.getWorksheet("Sheet1");
+
+  const output = await readExcelFile(sheet, searchText);
 
   if (!sheet) {
     console.log("Sheet not found");
     return;
   }
 
-  sheet.eachRow((row, rowNumber) => {
-    console.log(`Row ${rowNumber}:`);
-
-    row.eachCell((cell, colNumber) => {
-      console.log(`  Col ${colNumber}:`, cell.value);
-    });
-  });
+  const cellValue = sheet.getCell(output.row, output.column);
+  cellValue.value = replaceText;
+  await workbook.xlsx.writeFile(filePath);
 }
 
-readExcelFile();
+async function readExcelFile(sheet, searchText) {
+  let output = { row: 1, column: 1 };
+
+  sheet.eachRow((row, rowNumber) => {
+    // console.log(`Row ${rowNumber}:`);
+
+    row.eachCell((cell, colNumber) => {
+      // console.log(`  Col ${colNumber}:`, cell.value);
+
+      if (cell.value === searchText) {
+        output.row = rowNumber;
+        output.column = colNumber;
+      }
+    });
+  });
+  return output;
+}
+
+writeExcelFile(
+  "Banana",
+  "Republic",
+  "C:/Users/Acer/OneDrive/Desktop/excelDownload.xlsx"
+);
